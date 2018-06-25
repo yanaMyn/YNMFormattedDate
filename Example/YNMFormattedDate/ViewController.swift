b99c9a89fedb334b58b84161654318d6959b54aa
yana mulyana <fulan@MacBook-Pro-yana.local>
2018-06-25 14:59:10 +0700

    add example code

--- Example/YNMFormattedDate/ViewController.swift
+++ Example/YNMFormattedDate/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import YNMFormattedDate
 
 class ViewController: UIViewController {
 
+    var formatDate = YNMFormattedDate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let dateNow = self.formatDate.formattedDateFromString(dateFormat: "dd/MM/yyyy", dateString: "24/06/2018", withFormat: "MMM dd, yyyy")
+        print("today is => \(String(describing: dateNow))")
+        
     }
 
     override func didReceiveMemoryWarning() {
